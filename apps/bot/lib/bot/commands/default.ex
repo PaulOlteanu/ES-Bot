fed3484d8754db7465ad6ee96a84d07f55e1fc8a
Paul Olteanu <p.a.olteanu@gmail.com>
2020-06-04 18:05:14 -0400

    Add some commands

--- apps/bot/lib/bot/commands/default.ex
+++ apps/bot/lib/bot/commands/default.ex
@@ -4,6 +4,8 @@ defmodule Bot.Commands.Default do
   def run(msg, [command | _args]) do
     case String.downcase(command) do
       "!octavian" -> Api.create_message(msg.channel_id, "is a retard")
+      "!drop" -> Api.create_message(msg.channel_id, ":corn: spam :corn: this :corn: crop :corn: to :corn: get :corn: a :corn: drop")
+      "!key" -> Api.create_message(msg.channel_id, ":key: spam :key: this :key: key :key: to :key: get :key: a :key: tree")
       _ -> Api.create_message(msg.channel_id, "Unknown command")
     end
   end
